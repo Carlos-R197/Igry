230f40a8be7fa0695dd14d0920248a88a2b1b769
Carlos Roque <carlosroque198@gmail.com>
2020-12-17 09:47:21 -0400

    First commit. Project setup done

---
+++ Igry/Igry/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Prism;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace Igry
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) 
+        { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        
+    }
+}
