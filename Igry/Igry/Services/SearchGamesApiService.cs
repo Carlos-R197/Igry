b8c9787610d88eef59b51806169afd51a64384d4
Carlos Roque <carlosroque198@gmail.com>
2021-01-15 15:16:04 -0400

    Api service to search for games based on their name added

---
+++ Igry/Igry/Services/SearchGamesApiService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Igry.Models;
+using Newtonsoft.Json;
+
+namespace Igry.Services
+{
+    public class SearchGamesApiService
+    {
+        private const int amountResults = 10;
+
+        public async Task<IList<Game>> SearchAsync(string userInput)
+        {
+            IList<Game> games = null;
+            var client = new HttpClient();
+            string query = $"https://api.rawg.io/api/games?key=9b88a4289b784c19b41aff2c40764c6a&search={userInput}&page_size={amountResults}";
+            HttpResponseMessage response = await client.GetAsync(query);
+            if (response.IsSuccessStatusCode)
+            {
+                GamesList gameList = JsonConvert.DeserializeObject<GamesList>(await response.Content.ReadAsStringAsync());
+                games = gameList.Games;
+            }
+            return games;
+        }
+    }
+}
