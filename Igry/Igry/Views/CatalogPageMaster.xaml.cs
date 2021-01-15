cea11786d36e64f638c6ed15814d1532ace991cb
Muirghen <vasquezmariano01@gmail.com>
2021-01-15 09:42:36 -0400

    Work on Catalog filters

--- Igry/Igry/Views/CatalogPageMaster.xaml.cs
+++ Igry/Igry/Views/CatalogPageMaster.xaml.cs
@@ -11,64 +11,64 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Igry.Services;
 using Igry.Models;
+using Prism.Commands;
 
 namespace Igry.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CatalogPageMaster : ContentPage
-    {
-        public ListView ListView;
-        
+    {   
         public CatalogPageMaster()
         {
             InitializeComponent();
 
             BindingContext = new CatalogPageMasterViewModel();
-            ListView = MenuItemsListView;
         }
 
         class CatalogPageMasterViewModel : INotifyPropertyChanged
         {
+            IGetGenresApiService apiService = new GetGenresApiService();
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
             public ObservableCollection<CatalogPageMasterMenuItem> MenuItems { get; set; }
             public IList<Genre> GenresList { get; set; }
-            IGetGenresApiService apiService = new GetGenresApiService();
+            public IList<Genre> SelectedGenres { get; set; }
+            public DelegateCommand FilterCommand => new DelegateCommand(Filter);
+            public DelegateCommand AddGenreToListCommand => new DelegateCommand(AddGenreToList);
 
             public CatalogPageMasterViewModel()
             {
                LoadGenres();
-               //MenuItems = new ObservableCollection<CatalogPageMasterMenuItem>();
-               //for (int i = 0; i < 19; i++)
-               //{                   
-               //     CatalogPageMasterMenuItem menuItem = new CatalogPageMasterMenuItem { Id = GenresList[i].Id, Title = GenresList[i].Name};
-               //     MenuItems.Add(menuItem);
-               //}
-
-                //    new ObservableCollection<CatalogPageMasterMenuItem>(new[]
-                //{
-
-                //        new CatalogPageMasterMenuItem { Id = 0, Title = "Accion" },
-                //        new CatalogPageMasterMenuItem { Id = 1, Title = "Aventura" },
-                //        new CatalogPageMasterMenuItem { Id = 2, Title = "Blah" },
-                //        new CatalogPageMasterMenuItem { Id = 3, Title = "Page 4" },
-                //        new CatalogPageMasterMenuItem { Id = 4, Title = "Page 5" },
-                //    });
+                SelectedGenres = new List<Genre>();
             }
             public async void LoadGenres()
             {
                 var genreList = await apiService.GetGenres();
                 GenresList = genreList;
             }
-
-            #region INotifyPropertyChanged Implementation
-            public event PropertyChangedEventHandler PropertyChanged;
-            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            public void Filter()
             {
-                if (PropertyChanged == null)
-                    return;
-
-                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                if (SelectedGenres.Count!=0)
+                {
+                    var num = SelectedGenres.Count;
+                }
             }
-            #endregion
+            public void AddGenreToList()
+            {
+
+            }
+
+            //#region INotifyPropertyChanged Implementation
+            //public event PropertyChangedEventHandler PropertyChanged;
+            //void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            //{
+            //    if (PropertyChanged == null)
+            //        return;
+
+            //    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //}
+            //#endregion
         }
     }
 }
